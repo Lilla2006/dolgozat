afc21aebae45b8d9e285174419a8bfb7ac78f51a
Lilla <98951774+Lilla2006@users.noreply.github.com>
2024-11-19 08:46:39 +0100

    Add files via upload

---
+++ dolgozat.js
@@ -0,0 +1,36 @@
+function createArrayOfTiers(num) {
+    const Test = require('@codewars/test-compat');
+    for (let i = 0; i < Test.length; i++) {
+        
+        const element = Test[i] + 1; 
+        let num = element;
+    }
+    return num;
+}
+
+
+function myParseInt(str) {
+    if (NaN) {
+        let str = 0
+    }
+    return 0;
+}
+
+
+function disemvowel(str) {
+    const chai = require('chai'), { assert } = chai;
+    if (assert == "y") {
+      return str;
+    }
+    
+}
+
+
+function factorial(n){
+    const assert = require('chai').assert;
+    let sum = 1;
+    for (i = 0; i < n; i++) {
+      sum * i;
+    return sum;
+    }
+  }
